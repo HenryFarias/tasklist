826b9959ab871a48cd1a760ab2cf476b8b92f717
Henry Farias <henry.farias@jointecnologia.com.br>
2018-08-21 00:51:59 -0300

    dump da base

---
+++ dump.sql
@@ -0,0 +1,53 @@
+-- --------------------------------------------------------
+-- Servidor:                     127.0.0.1
+-- Versão do servidor:           10.1.30-MariaDB - mariadb.org binary distribution
+-- OS do Servidor:               Win32
+-- HeidiSQL Versão:              9.5.0.5196
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+-- Copiando estrutura do banco de dados para tasklist
+CREATE DATABASE IF NOT EXISTS `tasklist` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `tasklist`;
+
+-- Copiando estrutura para tabela tasklist.hibernate_sequence
+CREATE TABLE IF NOT EXISTS `hibernate_sequence` (
+  `next_val` bigint(20) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- Copiando dados para a tabela tasklist.hibernate_sequence: 1 rows
+/*!40000 ALTER TABLE `hibernate_sequence` DISABLE KEYS */;
+INSERT INTO `hibernate_sequence` (`next_val`) VALUES
+	(6);
+/*!40000 ALTER TABLE `hibernate_sequence` ENABLE KEYS */;
+
+-- Copiando estrutura para tabela tasklist.task
+CREATE TABLE IF NOT EXISTS `task` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `state` tinyint(1) NOT NULL,
+  `completed_at` datetime DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+-- Copiando dados para a tabela tasklist.task: ~3 rows (aproximadamente)
+/*!40000 ALTER TABLE `task` DISABLE KEYS */;
+INSERT INTO `task` (`id`, `title`, `description`, `state`, `completed_at`, `created_at`, `updated_at`, `deleted_at`) VALUES
+	(2, 'teste', 'teste', 1, '2018-08-21 00:00:00', NULL, '2018-08-21 00:00:00', NULL),
+	(4, 'teste', 'teste', 0, NULL, '2018-08-21 00:00:00', NULL, '2018-08-21 00:33:42'),
+	(5, 'teste', 'teste', 1, '2018-08-21 00:00:00', '2018-08-21 00:00:00', '2018-08-21 00:00:00', '2018-08-21 00:31:16');
+/*!40000 ALTER TABLE `task` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
